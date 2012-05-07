918d8c40fb3d84101ea70f9f5d6fd615756d0624
snipes83 <snipes83@2e2f1b09-eb66-4151-8dc0-6706b8fccda5>
2012-05-07 13:34:24 +0000

    Added methods for using observer pattern.

--- trunk/project/src/view/Gui.java
+++ trunk/project/src/view/Gui.java
@@ -1,6 +1,6 @@
 package view;
 
-import java.util.ArrayList;
+import java.util.Observable;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -8,28 +8,29 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class Gui {
+public class Gui extends Observable {
 	private static final String PROGRAM_TITLE = "Word Attack";
 	private static final String PROGRAM_VERSION = "0.1";
 
 	private JFrame frame;
+	private JTextField wordTextField;
 
 	public Gui() {
 		// Create components.
 		JLabel label1 = new JLabel("Word1");
-		JTextField field = new JTextField(20);
+		wordTextField = new JTextField(20);
 		JButton button1 = new JButton("Send");
 		
 		// Define positions.
 		label1.setBounds(100, 100, 100, 50);
-		field.setBounds(75, 100, 200, 25);
+		wordTextField.setBounds(75, 100, 200, 25);
 		button1.setBounds(40, 200, 75, 25);
 		
 		// Create panel.
 		JPanel panel = new JPanel();
 		panel.setLayout(null);
 		panel.add(label1);
-		panel.add(field);
+		panel.add(wordTextField);
 		panel.add(button1);
 		
 		// Create frame.
@@ -38,6 +39,10 @@ public class Gui {
 		frame.add(panel);
 		frame.setSize(800, 600);
 		frame.setVisible(true);
+	}
+	
+	public String getPlayerWord() {
+		return wordTextField.getText();
 	}
 	
 	// TODO
