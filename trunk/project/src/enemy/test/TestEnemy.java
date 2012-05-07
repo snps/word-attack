9125c92e9ec6ae512ba1a05288322817b7e36715
snipes83 <snipes83@2e2f1b09-eb66-4151-8dc0-6706b8fccda5>
2012-05-07 17:10:47 +0000

    Implemented tests for Enemy class.

---
+++ trunk/project/src/enemy/test/TestEnemy.java
@@ -0,0 +1,41 @@
+package enemy.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import enemy.Enemy;
+
+public class TestEnemy {
+	@Test
+	public void testCreateEnemy() {
+		Enemy enemy = new Enemy("Enemy", 10, 100);
+
+		assertEquals("Enemy", enemy.getWord());
+		assertEquals(10, enemy.getSpeed());
+		assertEquals(100, enemy.getXPos());
+		assertEquals(0, enemy.getYPos());
+		assertEquals(50, enemy.getWordWidth());
+	}
+
+	@Test
+	public void testMoveEnemy() {
+		Enemy enemy = new Enemy("Enemy", 10, 100);
+
+		assertEquals(0, enemy.getYPos());
+		enemy.move();
+		assertEquals(10, enemy.getYPos());
+	}
+
+	@Test
+	public void testEnemyEquals() {
+		Enemy enemy1 = new Enemy("Enemy", 10, 100);
+		Enemy enemy2 = new Enemy("Enemy", 5, 50);
+		Enemy enemy3 = new Enemy("Friend", 10, 100);
+
+		assertTrue(enemy1.equals(enemy2));
+		assertFalse(enemy1.equals(enemy3));
+	}
+}
