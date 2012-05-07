5442b9d2e821f9b4d700a5c5fe6165a6c6792308
johante <johante@2e2f1b09-eb66-4151-8dc0-6706b8fccda5>
2012-05-07 14:35:44 +0000

    Skeleton for Communication test.

---
+++ project/src/server/ClientListener.java
@@ -0,0 +1,13 @@
+package server;
+
+public class ClientListener extends Thread {
+
+	public ClientListener(WordMonitor words) {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void run() {
+		
+	}
+
+}
---
+++ project/src/server/WordMonitor.java
@@ -0,0 +1,10 @@
+package server;
+
+public class WordMonitor {
+
+	public Object getWord() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
