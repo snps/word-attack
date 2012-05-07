d7436d1d6268f0d86af321ec7fd2de8d841196fb
Felix Glas <snps@users.noreply.github.com>
2012-05-07 13:34:24 +0000

    Added methods for using observer pattern.

--- project/src/client/Client.java
+++ project/src/client/Client.java
@@ -1,5 +1,22 @@
 package client;
 
-public class Client {
+import java.util.Observable;
+import java.util.Observer;
+
+import view.Gui;
+
+public class Client implements Observer {
+	private Gui gui;
+	
+	public Client(Gui gui) {
+		this.gui = gui;
+	}
+
+	@Override
+	public void update(Observable obs, Object o) {
+		String word = gui.getPlayerWord();
+		
+		// TODO
+	}
 
 }
