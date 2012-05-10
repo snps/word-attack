4fe49f1efaa9116831e4ce910156a330fafa85d6
snipes83 <snipes83@2e2f1b09-eb66-4151-8dc0-6706b8fccda5>
2012-05-10 17:16:22 +0000

    Fixed ClientTest to comply to recent changes.

--- trunk/project/src/client/test/ClientTest.java
+++ trunk/project/src/client/test/ClientTest.java
@@ -7,10 +7,16 @@ import client.Client;
 public class ClientTest {
 	public static void main(String[] args) {
 		Client client = new Client("Player 1");
-
+		
 		ServerStub stub = new ServerStub(4444);
 		stub.setDaemon(true);
 		stub.start();
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e1) {
+			System.err.println("Interruped!");
+		}
 
 		try {
 			client.connect("localhost", 4444);
