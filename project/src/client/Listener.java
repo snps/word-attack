55772fea9f9ca6affc702589704d78fe4da3c0f9
Felix Glas <snps@users.noreply.github.com>
2012-05-10 14:30:20 +0000

    Changed name of ServerListener to simply "Listener" (already in package "client" so there is no confusion). Implemented Listener methods.

---
+++ project/src/client/Listener.java
@@ -0,0 +1,72 @@
+package client;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import net.NetPacket;
+import net.NetPacketReader;
+import net.NetPacketReaderTimeoutException;
+
+public class Listener extends Thread {
+	public static final int READ_INTERVAL = 300;
+
+	private Client client;
+	private InputStream input;
+
+	public Listener(Client client, InputStream input) {
+		this.client = client;
+		this.input = input;
+	}
+
+	@Override
+	public void run() {
+		NetPacketReader reader = new NetPacketReader(input, READ_INTERVAL);
+
+		while (!isInterrupted()) {
+			NetPacket packet = null;
+
+			// Read next packet from server.
+			try {
+				packet = reader.readPacket();
+			} catch (NetPacketReaderTimeoutException e) {
+				continue;
+			} catch (IOException e) {
+				System.err.println("Client listener could not read packet from server!");
+			}
+
+			// Handle packet.
+			handlePacket(packet);
+		}
+	}
+
+	private void handlePacket(NetPacket packet) {
+		NetPacket.Type type = packet.getType();
+
+		if (type == NetPacket.Type.CREATE_ENEMY) {
+			String word = packet.getPacketElementContent(NetPacket.WORD_TAG);
+			int speed = Integer.parseInt(packet.getPacketElementContent(NetPacket.SPEED_TAG));
+			int xPos = Integer.parseInt(packet.getPacketElementContent(NetPacket.X_POS_TAG));
+
+			client.createEnemy(word, speed, xPos);
+		} else if (type == NetPacket.Type.DESTROY_ENEMY) {
+			String word = packet.getPacketElementContent(NetPacket.WORD_TAG);
+			String playerName = packet.getPacketElementContent(NetPacket.SCORING_PLAYER_TAG);
+
+			client.destroyEnemy(word);
+			client.increaseScore(playerName);
+		} else if (type == NetPacket.Type.MOVE_ENEMIES) {
+			client.moveEnemies();
+		} else if (type == NetPacket.Type.GAME_OVER) {
+			// Disconnect from server.
+			try {
+				client.disconnect();
+			} catch (IOException e) {
+				System.err.println("Could not disconnect from server!");
+			}
+			
+			client.showMessage("Game Over");
+		} else if (type == NetPacket.Type.UNKNOWN) {
+			client.showMessage("Unknown packet received from server");
+		}
+	}
+}
