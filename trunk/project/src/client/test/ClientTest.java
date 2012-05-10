1fd8a5a6b4daa040daebdf5b17b0b93f20de18af
snipes83 <snipes83@2e2f1b09-eb66-4151-8dc0-6706b8fccda5>
2012-05-10 14:52:16 +0000

    Edited client tests to conform to new changes.

--- trunk/project/src/client/test/ClientTest.java
+++ trunk/project/src/client/test/ClientTest.java
@@ -1,13 +1,21 @@
 package client.test;
 
+import java.io.IOException;
+
 import client.Client;
 
 public class ClientTest {
 	public static void main(String[] args) {
 		Client client = new Client("Player 1");
-		
-		ServerStub stub = new ServerStub(client);
+
+		ServerStub stub = new ServerStub(4444);
 		stub.isDaemon();
 		stub.start();
+
+		try {
+			client.connect("localhost", 4444);
+		} catch (IOException e) {
+			System.err.println("Could not connect to server!");
+		}
 	}
 }
