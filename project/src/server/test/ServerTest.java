4ae36a647790f3e1184d9ae10474d0279e67aca4
Felix Glas <snps@users.noreply.github.com>
2012-05-12 15:48:49 +0000

    Added support for server to automatically shut down when there are no client connections left. Added a server socket accept timeout (server continuously calls accept and then timeouts -> accept -> timeout...).

--- project/src/server/test/ServerTest.java
+++ project/src/server/test/ServerTest.java
@@ -1,10 +1,18 @@
 package server.test;
 
+import java.io.IOException;
+
 import server.Server;
 
 public class ServerTest {
 	public static void main(String[] args) {
-		Server server = new Server(4444, 2);
+		Server server = null;
+		try {
+			server = new Server(4444, 2);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		server.start();
 	}
 }
