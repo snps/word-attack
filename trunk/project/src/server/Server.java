47aaee97476e8a5a64eef66a18eedf41cd9aacd7
snipes83 <snipes83@2e2f1b09-eb66-4151-8dc0-6706b8fccda5>
2012-05-12 15:48:49 +0000

    Added support for server to automatically shut down when there are no client connections left. Added a server socket accept timeout (server continuously calls accept and then timeouts -> accept -> timeout...).

--- trunk/project/src/server/Server.java
+++ trunk/project/src/server/Server.java
@@ -3,17 +3,24 @@ package server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
 
 import net.NetPacket;
 
 public class Server extends Thread {
 	public static final int MAX_CLIENTS = 4;
+	public static final int SERVER_ACCEPT_TIMEOUT = 1000;
 
 	private int port;
 	private int nbrOfPlayers;
 	private ClientMonitor clientMonitor;
 
-	public Server(int port, int nbrOfPlayers) {
+	public Server(int port, int nbrOfPlayers) throws IOException {
+		// Test if port is available.
+		ServerSocket server = new ServerSocket(port);
+		server.close();
+
 		this.port = port;
 		this.nbrOfPlayers = nbrOfPlayers;
 		clientMonitor = new ClientMonitor();
@@ -21,20 +28,40 @@ public class Server extends Thread {
 
 	@Override
 	public void run() {
-		while (!isInterrupted()) {
+		boolean hasStarted = false;
+
+		ServerSocket server = null;
+		outer: while (!isInterrupted()) {
+			System.out.println("Server listening for connections on port " + port);
+			
 			while (!isInterrupted() && clientMonitor.size() < MAX_CLIENTS) {
-				ServerSocket server = null;
+				// Interrupt when no clients connections left.
+				if (hasStarted && clientMonitor.size() == 0) {
+					break outer;
+				}
+
 				try {
 					server = new ServerSocket(port);
 				} catch (IOException e) {
 					System.err.println("Server could not listen on port " + port);
+					break outer;
 				}
 
-				System.out.println("Server listening for connections on port " + port);
+				try {
+					server.setSoTimeout(SERVER_ACCEPT_TIMEOUT);
+				} catch (SocketException e) {
+					System.err.println("Could not set server socket timeout");
+				}
 
 				try {
-					Socket socket = server.accept();
-					server.close();
+					Socket socket = null;
+					try {
+						socket = server.accept();
+					} catch (SocketTimeoutException e) {
+						continue;
+					} finally {
+						server.close();
+					}
 
 					ClientConnection connection = new ClientConnection(clientMonitor, socket);
 					clientMonitor.addClientConnection(connection);
@@ -49,8 +76,12 @@ public class Server extends Thread {
 				} catch (IOException e) {
 					System.err.println("Server could not open connection to client!");
 				}
+
+				hasStarted = true;
 			}
 			clientMonitor.waitForDisconnect();
 		}
+
+		System.out.println("Server shutting down");
 	}
 }
