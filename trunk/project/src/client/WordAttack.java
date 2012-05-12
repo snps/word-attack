ee7ad46747ab17ce45d6728c6b28c5a2cc27fe12
snipes83 <snipes83@2e2f1b09-eb66-4151-8dc0-6706b8fccda5>
2012-05-12 15:56:21 +0000

    Implemented a ConnectBox which is a Gui for joining/hosting games. Implemented an entry point class WordAttack that contains the game main method.

---
+++ trunk/project/src/client/WordAttack.java
@@ -0,0 +1,10 @@
+package client;
+
+import view.ConnectBox;
+
+public class WordAttack {
+	public static void main(String[] args) {
+		@SuppressWarnings("unused")
+		ConnectBox box = new ConnectBox();
+	}
+}
