cab2eee9983ce80a7543f5d89d7aa38a90ea1725
snipes83 <snipes83@2e2f1b09-eb66-4151-8dc0-6706b8fccda5>
2012-05-14 10:42:41 +0000

    Added two tests for very long word lists.

--- trunk/project/src/wordlist/test/TestWordChooser.java
+++ trunk/project/src/wordlist/test/TestWordChooser.java
@@ -15,29 +15,20 @@ import wordlist.WordChooser;
 import wordlist.WordFileReader;
 
 public class TestWordChooser {
-	public static final String WORD_FILE1 = "words/words1.txt";
-	public static final String WORD_FILE2 = "words/words2.txt";
+	public static final String WORD_FILE_1 = "words/words1.txt";
+	public static final String WORD_FILE_2 = "words/words2.txt";
+	public static final String WORD_FILE_3 = "words/words3.txt";
 
 	private List<String> words1;
 	private List<String> words2;
+	private List<String> words3;
 
 	@Before
 	public void setUp() {
-		// Read words into list from file.
-		WordFileReader wordReader = null;
-		try {
-			wordReader = new WordFileReader(WORD_FILE1);
-		} catch (FileNotFoundException e) {
-			System.err.println("File not found " + WORD_FILE1);
-		}
-		words1 = wordReader.readWords();
-
-		try {
-			wordReader = new WordFileReader(WORD_FILE2);
-		} catch (FileNotFoundException e) {
-			System.err.println("File not found " + WORD_FILE2);
-		}
-		words2 = wordReader.readWords();
+		// Read word lists from word files.
+		words1 = getWordlistFromFile(WORD_FILE_1);
+		words2 = getWordlistFromFile(WORD_FILE_2);
+		words3 = getWordlistFromFile(WORD_FILE_3);
 	}
 
 	@Test
@@ -92,5 +83,46 @@ public class TestWordChooser {
 		}
 
 		assertEquals("Gave wrong number of words", 2, nbr);
+	}
+	
+	@Test
+	public void testGetCorrectNbrOfWordsFromLongList() {
+		WordChooser wc = new WordChooser(words3);
+
+		int nbr = 0;
+
+		while (wc.hasAvailableWord()) {
+			wc.getNextWord();
+			nbr++;
+		}
+
+		assertEquals("Gave wrong number of words", 24, nbr);
+	}
+	
+	@Test
+	public void testGiveBackWordWithLongList() {
+		WordChooser wc = new WordChooser(words3);
+
+		String word = "";
+		for (int i = 0; i < 24; i++) {
+			word = wc.getNextWord();
+		}
+
+		assertFalse("Gave more words then possible", wc.hasAvailableWord());
+
+		wc.giveBackWord(word);
+		assertTrue("No words available although a word was given back", wc.hasAvailableWord());
+		assertFalse("Word was null", wc.getNextWord() == null);
+	}
+	
+	private List<String> getWordlistFromFile(String fileName) {
+		WordFileReader wordReader = null;
+		try {
+			wordReader = new WordFileReader(fileName);
+		} catch (FileNotFoundException e) {
+			System.err.println("File not found " + fileName);
+			return null;
+		}
+		return wordReader.readWords();
 	}
 }
